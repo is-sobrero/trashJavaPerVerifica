e8dc6089ddb28fac5e9d51a1311c9339e047c9a9
Ettore L. Pane <ettore.pane4@gmail.com>
2021-11-08 16:40:23 +0100

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        //dichiara 3 aerei 
+        Aereo aereo1 = new Aereo("TRIGEAR XS", "ABC123", 4000, 400, 1);
+        Aereo aereo2 = new Aereo("Cessna413", "ZDC123", 4000, 400, 2);
+        Aereo aereo3 = new Aereo("Tupolov", "HDK432", 4000, 400, 3);
+        
+
     }
 }
